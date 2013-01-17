be8c235aac354d839d3906023b08ade50bd01960
Marcin <marcin.nowak@itcrowd.pl>
2013-01-17 10:43:07 +0100

    Empty Class

---
+++ src/main/java/pl/itcrowd/tutorials/ejb/BeanReaderException.java
@@ -0,0 +1,11 @@
+package pl.itcrowd.tutorials.ejb;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kleks
+ * Date: 1/17/13
+ * Time: 10:41 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BeanReaderException {
+}
---
+++ src/main/java/pl/itcrowd/tutorials/ejb/MySingleton.java
@@ -0,0 +1,11 @@
+package pl.itcrowd.tutorials.ejb;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kleks
+ * Date: 1/17/13
+ * Time: 10:34 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MySingleton {
+}
